38e49f9a6cf21d0b9e746f81f571cd4d4d7404b7
root <alex.conev@gmail.com>
2015-11-21 10:08:19 +0200

    New Rel

--- application/models/Sms_model.php
+++ application/models/Sms_model.php
@@ -9,7 +9,11 @@ class Sms_model extends CI_Model {
         
         $page = $this->uri->segment(3) == 'page' ? $this->uri->segment(4) : 1 ;
 
-        $query = $this->db->limit($nPerPage, ($page-1)*$nPerPage)->get('sms_inbox');
+        $query = $this->db->select('IFNULL(phonebook.Name, sms_inbox.Sender) AS Sender, sms_inbox.ID, sms_inbox.Content, sms_inbox.Date')
+                          ->from('sms_inbox')
+                          ->join('phonebook', 'phonebook.Phone LIKE CONCAT("%",SUBSTR(sms_inbox.Sender,5,9))', 'left')
+                          ->limit($nPerPage, ($page-1)*$nPerPage)
+                          ->order_by('sms_inbox.Date', "DESC")->get();
         return $query->result_array();
 	}   
 
@@ -23,7 +27,12 @@ class Sms_model extends CI_Model {
 
         $page = $this->uri->segment(3) == 'page' ? $this->uri->segment(4) : 1 ;
 
-        $query = $this->db->limit($nPerPage, ($page-1)*$nPerPage)->get('sms_outbox');
+        $query = $this->db->select('IFNULL(phonebook.Name, sms_outbox.Recipient) AS Recipient, sms_outbox.ID, sms_outbox.Content, sms_outbox.Date')
+                          ->from('sms_outbox')
+                          ->join('phonebook', 'phonebook.Phone LIKE CONCAT("%",SUBSTR(sms_outbox.Recipient,5,9))', 'left')
+                          ->order_by('sms_outbox.Date', "DESC")
+                          ->limit($nPerPage, ($page-1)*$nPerPage)
+                          ->order_by('sms_outbox.Date', "DESC")->get();
         return $query->result_array();
     }  
 
